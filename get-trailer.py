9a119f87af239b413365909f178edc5018157c83
Chenca <renato.chencarek@openbossa.org>
2009-12-02 16:53:23 -0300

    Adding support to 720P videos

--- get-trailer.py
+++ get-trailer.py
@@ -3,7 +3,7 @@
 import cgi
 import curl
 import re
-
+import ConfigParser
 
 def cgi_params():
     storage = cgi.FieldStorage()
@@ -13,14 +13,22 @@ def cgi_params():
     return params
 
 params = cgi_params()
-
 uid = params['uid']
+
+config = ConfigParser.ConfigParser()
+config.read('config.ini')
+
+hd = config.getboolean('trailers', 'hd')
 
 uri = "http://www.keep-youtube.com/watch?v=%s" % str(uid)
 crl = curl.Curl(uri)
 data = crl.get()
 crl.close()
 
-url = re.findall("http.*fmt\%3D18", data)[0]
+if hd:
+    url = re.findall("http.*fmt\%3D22", data)[0]
+else:
+    url = re.findall("http.*fmt\%3D18", data)[0]
+
 print "Location: %s" % ("http://keep-tube.com/dl.php?r=" + url)
 print
